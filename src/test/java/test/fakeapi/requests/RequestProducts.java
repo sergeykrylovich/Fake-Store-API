c772fb6947dc6d9e9aa7d444326133d700f557a8
sergeikrylovich <sergeybumbum@gmail.com>
2023-10-22 00:12:59 +0300

    added method to delete single product by API

--- src/test/java/test/fakeapi/requests/RequestProducts.java
+++ src/test/java/test/fakeapi/requests/RequestProducts.java
@@ -42,14 +42,41 @@ public class RequestProducts {
                                       List<String> images) {
 
         installSpecification(requestSpecification(BASEPATH), responseSpecification201());
+        CreateProductPOJO createProductPOJO = new CreateProductPOJO(title, price, description, categoryId, images);
+        return given()
+                .body(createProductPOJO)
+                .when()
+                .post("/")
+                .then()
+                //.log().all()
+                .extract().jsonPath().getObject("", ProductsPOJO.class);
+    }
+
+    public ProductsPOJO updateProduct(Integer productId, String title, Integer price,
+                                      String description, Integer categoryId,
+                                      List<String> images) {
+
+        installSpecification(requestSpecification(BASEPATH), responseSpecification201());
 
         return given()
                 .body(new CreateProductPOJO(title, price, description, categoryId, images))
                 .when()
-                .post("/")
+                .put("/" + productId)
                 .then()
                 .log().all()
                 .extract().jsonPath().getObject("", ProductsPOJO.class);
+    }
+
+    public String deleteSingleProduct(Integer productId) {
+
+        installSpecification(requestSpecification(BASEPATH), responseSpecification200());
+
+        return given()
+                .when()
+                .delete("/" + productId)
+                .then()
+                .log().all()
+                .extract().htmlPath().get("html.body");
     }
 
 
