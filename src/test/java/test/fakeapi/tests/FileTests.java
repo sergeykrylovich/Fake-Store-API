6ee4f048ded3c129fccbaa59bac10cd6aa4670da
sergeykrylovich <sergeykrylovich@gmail.com>
2023-12-17 23:17:23 +0300

    trying to compare 2 files

--- src/test/java/test/fakeapi/tests/FileTests.java
+++ src/test/java/test/fakeapi/tests/FileTests.java
@@ -1,8 +1,13 @@
 package test.fakeapi.tests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.qameta.allure.Epic;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
 import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.Test;
 import test.fakeapi.pojo.CategoryPOJO;
@@ -10,11 +15,18 @@ import test.fakeapi.requests.RequestFiles;
 import test.fakeapi.utils.JsonHelper;
 
 import java.io.File;
+import java.io.FileInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static test.fakeapi.specs.Constants.BASE_URL;
 
+@Epic("Endpoints for working with files")
 public class FileTests {
+
+    @BeforeAll
+    public static void setAllure() {
+        RestAssured.filters(new AllureRestAssured());
+    }
 
     @Test
     public void testFromFileJsonToObject() {
@@ -70,5 +82,14 @@ public class FileTests {
             assertThat(responseFileName).endsWith(fileExtension);
             assertThat(responseBody.getString("location")).isEqualTo(BASE_URL + "/files/" + responseFileName);
         });
+    }
+
+    @Test
+    @SneakyThrows
+    public void testGetFile() {
+        FileInputStream fileInputStream = new FileInputStream(new File("src/test/resources/file.json"));
+        FileInputStream fileInputStream1 = new FileInputStream(new File("src/test/resources/file1.json"));
+        assertThat(IOUtils.contentEquals(fileInputStream, fileInputStream1)).isTrue();
+
     }
 }
